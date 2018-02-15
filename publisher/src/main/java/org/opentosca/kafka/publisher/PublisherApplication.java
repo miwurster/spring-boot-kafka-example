a131d731e66374f7ad70dc8d57014e7dfcfa7f6b
Michael Wurster <miwurster@googlemail.com>
2018-02-15 12:46:48 +0100

    Initial commit

---
+++ publisher/src/main/java/org/opentosca/kafka/publisher/PublisherApplication.java
@@ -0,0 +1,50 @@
+package org.opentosca.kafka.publisher;
+
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@SpringBootApplication
+public class PublisherApplication implements CommandLineRunner {
+
+  private static Logger logger = LoggerFactory.getLogger(PublisherApplication.class);
+
+  public static void main(final String[] args) {
+    SpringApplication.run(PublisherApplication.class, args);
+  }
+
+  // -- Main program
+
+  private final KafkaTemplate<Integer, String> kafkaTemplate;
+
+  @Autowired
+  public PublisherApplication(final KafkaTemplate<Integer, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  @Override
+  public void run(final String... strings) throws Exception {
+    while (true) {
+      ListenableFuture<SendResult<Integer, String>> f;
+
+      f = kafkaTemplate
+          .send("my.awesome.hello", "Hello");
+      f.get();
+      logger.info("1. message sent? {}", f.isDone());
+
+      f = kafkaTemplate
+          .send("my.awesome.world", "World.");
+      f.get();
+      logger.info("2. message sent? {}", f.isDone());
+
+      Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+    }
+  }
+}
